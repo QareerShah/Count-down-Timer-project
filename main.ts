d8c45b2561f5a812c29f601720f9d3c3e551d8cf
QareerShah <queensyeda16@gmail.com>
2024-05-15 13:19:25 +0500

    Adding a Cli based countdown timer project

---
+++ main.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"; 
+import chalk from "chalk";
+import {differenceInSeconds} from "date-fns";
+
+
+console.log(chalk.bold.yellowBright("\n\t\tWelcome to Qareer-ul-Ain  'Countdown Timer'\n\t\t "));
+
+
+const res = await inquirer.prompt({
+    name : "UserInput",
+    type : "number",
+    message : "Please enter the amount of seconds",
+    validate: (input) => {
+        if(isNaN(input)){
+            return "Please enter valid number"
+        } else if (input > 60){
+            return "seconds must be in 60"
+        }else{
+            return true;
+        }
+    }
+});
+
+let input = res.UserInput
+
+function startTime(val: number){
+    const inTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(inTime);
+    setInterval((()=>{
+        const currentTime = new Date()
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+
+        if(timeDiff <= 0){
+            console.log("Timer has Expired!");
+            process.exit()
+        }
+        const min = Math.floor((timeDiff%(3600*24)/3600))
+        const sec = Math.floor(timeDiff%60)
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }),1000)
+}
+
+startTime(input);
